b740b7b78c1a80d428e3902806125fae6cf0f510
Hector5280g <hectorgarcia342@yahoo.com>
2022-10-12 14:53:09 -0600

    if and statements

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+//console.log("Hello World");
+let num= -0; 
+
+if(num > 0 ) {
+    console.log("positive")
+} else if(num < 0 ) {
+    console.log("negative")
+} else {
+    console.log("not negative or positive");
+}
+//ACCESS GRANTED
+
+const allowedAge = 20;
+
+if(allowedAge >= 18 ) {
+    console.log("access granted. welcome aboard!")
+} else {
+    console.log("You are too young to access this content")
+}
